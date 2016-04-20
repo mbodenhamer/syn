071afb888b24b0383d8d9022d1c37b27edb865aa
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-04-20 18:44:12 -0500

    Adding initial base/b/Base code (and tests)

--- syn/base/b/base.py
+++ syn/base/b/base.py
@@ -1,6 +1,6 @@
 import six
 from .meta import Attrs, Meta
-from syn.base_utils import AttrDict, ReflexiveDict
+from syn.base_utils import AttrDict, ReflexiveDict, message
 
 #-------------------------------------------------------------------------------
 # Base
@@ -16,6 +16,115 @@ class Base(object):
                      id_equality = False,
                      init_validate = False,
                      optional_none = False)
+
+    def __init__(self, *args, **kwargs):
+        _args = self._opts.args
+
+        for key in self._attrs.defaults:
+            if key in _args:
+                if len(args) > _args.index(key):
+                    continue # This value has been supplied as a non-kw arg
+            if key not in kwargs:
+                kwargs[key] = self._attrs.defaults[key]
+        
+        if _args:
+            if len(args) > len(_args):
+                raise TypeError('__init__ takes up to {} positional arguments '
+                                '({} given)'.format(len(_args), len(args)))
+
+            for k, arg in enumerate(args):
+                key = _args[k]
+                if key in kwargs:
+                    raise TypeError('__init__ got multiple values for argument '
+                                    '{}'.format(key))
+                kwargs[_args[k]] = arg
+
+        if self._opts.coerce_args:
+            for key, value in list(kwargs.items()):
+                typ = self._attrs.types[key]
+                if not typ.query(value):
+                    kwargs[key] = typ.coerce(value)
+
+        if self._opts.optional_none:
+            for attr in self._attrs.optional:
+                if attr not in kwargs:
+                    kwargs[attr] = None
+
+        for attr, call in self._attrs.call:
+            value = kwargs.get(attr, None)
+            if value is None:
+                kwargs[attr] = call()
+            else:
+                kwargs[attr] = call(value)
+
+        self.__setstate__(kwargs)
+
+        if self._opts.init_validate:
+            self.validate()
+
+    def __getstate__(self):
+        return self.to_dict()
+
+    def __setstate__(self, state):
+        for attr, val in state.items():
+            setattr(self, attr, val)
+
+    def __eq__(self, other):
+        if self._opts.id_equality:
+            return self is other
+
+        if type(self) is not type(other):
+            return False
+
+        dct1 = self.to_dict()
+        dct2 = other.to_dict()
+        return dct1 == dct2
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def to_dict(self, *groups, **kwargs):
+        '''Convert the object into a dict of its declared attributes.
+        
+        May exclude certain attribute groups by listing them in *groups.
+        
+        May include certain attribute groups (to the exclusion of all others) by listing them in *groups and supplying the include=True keyword argument.
+        '''
+        include = kwargs.get('include', False)
+
+        if not include:
+            if groups:
+                exclude = self._groups.union(*groups)
+            else:
+                exclude = set()
+        else:
+            exclude = self._groups.complement(*groups)
+
+        return {attr: getattr(self, attr) for attr in self._attrs.types
+                if attr not in exclude and hasattr(self, attr)}
+
+    def validate(self):
+        '''Raise an exception if the object is missing required attributes, or if the attributes are of an invalid type.
+        '''
+        optional = self._attrs.optional
+        optional_none = self._opts.optional_none
+
+        for attr, typ in self._attrs.types.items():
+            if not hasattr(self, attr):
+                if attr in optional:
+                    continue
+                raise AttributeError('Required attribute {} not defined'.
+                                     format(attr))
+
+            val = getattr(self, attr)
+            if optional_none:
+                if attr in optional and val is None:
+                    continue
+
+            res, e = typ.query_exception(val)
+            if not res:
+                raise TypeError('Validation error for attribute {}: {}'.
+                                format(attr, message(e)))
 
 
 #-------------------------------------------------------------------------------
--- syn/base/b/tests/test_base.py
+++ syn/base/b/tests/test_base.py
@@ -1,13 +1,91 @@
+import six
 from nose.tools import assert_raises
 from syn.base.b import Base, Attr
 from syn.base_utils import assert_equivalent, assert_pickle_idempotent, \
     assert_deepcopy_idempotent, assert_inequivalent
 
+if six.PY2:
+    str = unicode
+
 #-------------------------------------------------------------------------------
 # Test basic functionality
 
+class A(Base):
+    _attrs = dict(a = Attr(int),
+                  b = Attr(float),
+                  c = Attr(str, optional=True))
+    _opts = dict(init_validate = True)
+
+
 def test_base():
-    pass
+    kwargs = dict(a=5, b=3.4, c=u'abc')
+    obj = A(**kwargs)
+
+    assert obj.a == 5
+    assert obj.b == 3.4
+    assert obj.c == u'abc'
+    
+    assert obj.to_dict() == kwargs
+    #assert obj.to_dict(exclude=['a', 'b']) == dict(c=u'abc')
+
+    assert obj != 5
+    assert_equivalent(obj, A(**kwargs))
+    assert_inequivalent(obj, A(a=6, b=3.4, c=u'abc'))
+
+    assert A(a=5, b=3.4).to_dict() == dict(a=5, b=3.4)
+
+    assert_deepcopy_idempotent(obj)
+    assert_pickle_idempotent(obj)
+
+    assert_raises(TypeError, A, a=5.1, b=3.4)
+    assert_raises(AttributeError, A, a=5)
+
+#-------------------------------------------------------------------------------
+# Test Positional Args
+
+class B(A):
+    _attrs = dict(b = Attr(float, default=1.2))
+    _opts = dict(args = ('a', 'b'))
+
+def test_positional_args():
+    obj = B(5, 3.4, c=u'abc')
+
+    assert obj.a == 5
+    assert obj.b == 3.4
+    assert obj.c == u'abc'
+
+    assert B(5).to_dict() == dict(a=5, b=1.2)
+
+    assert_raises(TypeError, B, 1, 2, 3)
+
+#-------------------------------------------------------------------------------
+# Test arg coercion
+
+class C(B):
+    _opts = dict(coerce_args = True)
+
+def test_arg_coercion():
+    obj = C(5.1, 3)
+    
+    assert obj.a == 5
+    assert obj.b == 3.0
+
+    assert isinstance(obj.a, int)
+    assert isinstance(obj.b, float)
+
+#-------------------------------------------------------------------------------
+# Test optional None
+
+class D(C):
+    _opts = dict(optional_none = True)
+
+def test_optional_none():
+    obj = D(5, 3.4)
+
+    assert obj.a == 5
+    assert obj.b == 3.4
+    assert obj.c is None
+    obj.validate()
 
 #-------------------------------------------------------------------------------
 
