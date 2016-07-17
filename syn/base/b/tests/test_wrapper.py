bc15ae0033dc8beb0cce7abd6c291e90abd41450
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-07-16 22:33:32 -0500

    Adding init ListWrapper code and tests

--- syn/base/b/tests/test_wrapper.py
+++ syn/base/b/tests/test_wrapper.py
@@ -1,10 +1,60 @@
+import collections
+from copy import deepcopy
+from nose.tools import assert_raises
 from syn.base.b import ListWrapper
+from syn.base.b.tests.test_base import check_idempotence
 
 #-------------------------------------------------------------------------------
 # ListWrapper
 
 def test_listwrapper():
-    ListWrapper
+    obj = ListWrapper(1, 2, 3)
+    check_idempotence(obj)
+
+    assert isinstance(obj, collections.MutableSequence)
+    assert issubclass(ListWrapper, collections.MutableSequence)
+
+    cobj = deepcopy(obj)
+    _list = list(range(10))
+    cobj._list = list(_list)
+
+    assert list(cobj) == _list
+    cobj.append(11)
+    assert list(cobj) == _list + [11]
+    cobj.extend([12,13])
+    assert list(cobj) == _list + [11, 12, 13]
+    cobj.insert(0, 14)
+    assert list(cobj) == [14] + _list + [11, 12, 13]
+    cobj.pop()
+    assert list(cobj) == [14] + _list + [11, 12]
+    cobj.remove(12)
+    assert list(cobj) == [14] + _list + [11]
+
+    assert cobj.count(1) == 1
+    assert cobj.index(0) == 1
+
+    cobj.sort()
+    assert list(cobj) == _list + [11, 14]
+
+    assert cobj[-1] == 14
+    cobj[-1] = 15
+    assert list(cobj) == _list + [11, 15]
+    del cobj[-1]
+
+    cobj.reverse()
+    _list.reverse()
+    assert list(cobj) == [11] + _list
+    
+    class LW1(type(obj)):
+        _max_len = 1
+        _min_len = None
+
+    class LW2(type(obj)):
+        _max_len = None
+        _min_len = 50
+
+    assert_raises(ValueError, LW1(_list=_list).validate)
+    assert_raises(ValueError, LW2(_list=_list).validate)
 
 #-------------------------------------------------------------------------------
 
--- syn/base/b/wrapper.py
+++ syn/base/b/wrapper.py
@@ -1,13 +1,99 @@
+import collections
+
 from .base import Base
 from .meta import Attr
+
+#-------------------------------------------------------------------------------
+# Constants
+
+_LIST = '_list'
 
 #-------------------------------------------------------------------------------
 # ListWrapper
 
 
 class ListWrapper(Base):
-    _attrs = dict()
+    _attrs = dict(_list = Attr(list, doc='The wrapped list'))
+    _opts = dict(max_len = None,
+                 min_len = None)
+    
+    def __init__(self, *args, **kwargs):
+        max_len = self._opts.max_len
+        ltype = self._attrs.types[_LIST]
+        _args = self._opts.args
 
+        if max_len is None or not _args:
+            _list = ltype.coerce(args)
+            args = ()
+        else:
+            _list = ltype.coerce(args[:max_len])
+            args = args[max_len:]
+
+        _list.extend(kwargs.get(_LIST, ltype.coerce([])))
+
+        kwargs[_LIST] = _list
+        super(ListWrapper, self).__init__(*args, **kwargs)
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __len__(self):
+        return len(self._list)
+
+    def __getitem__(self, item):
+        return self._list[item]
+
+    def __setitem__(self, item, value):
+        self._list[item] = value
+
+    def __delitem__(self, item):
+        del self._list[item]
+
+    def append(self, item):
+        self._list.append(item)
+
+    def count(self, item):
+        return self._list.count(item)
+
+    def extend(self, items):
+        self._list.extend(items)
+
+    def index(self, item):
+        return self._list.index(item)
+
+    def insert(self, index, item):
+        self._list.insert(index, item)
+
+    def pop(self, index=-1):
+        self._list.pop(index)
+
+    def remove(self, item):
+        self._list.remove(item)
+
+    def reverse(self):
+        self._list.reverse()
+
+    def sort(self, *args, **kwargs):
+        self._list.sort(*args, **kwargs)
+
+    def validate(self):
+        super(ListWrapper, self).validate()
+        
+        max_len = type(self)._max_len
+        min_len = type(self)._min_len
+
+        if max_len is not None:
+            if len(self._list) > max_len:
+                raise ValueError("can have at most %d elemsents; (got %d)"
+                                 % (max_len, len(self._list)))
+
+        if min_len is not None:
+            if len(self._list) < min_len:
+                raise ValueError("must have at least %d elemsents; (got %d)"
+                                 % (min_len, len(self._list)))
+        
+
+collections.MutableSequence.register(ListWrapper)
 
 #-------------------------------------------------------------------------------
 # __all__
