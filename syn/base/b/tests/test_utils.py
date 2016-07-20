c87081a5d945e6a4e5fbfef9aced61638e698f2e
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-07-20 00:49:40 -0500

    Adding syn.base.b.utils.Counter and tests

--- syn/base/b/tests/test_utils.py
+++ syn/base/b/tests/test_utils.py
@@ -1,10 +1,33 @@
+from nose.tools import assert_raises
 from syn.base.b import Counter
 
 #-------------------------------------------------------------------------------
 # Counter
 
 def test_counter():
-    Counter
+    c = Counter()
+    c()
+    assert c.peek() == 0
+    for i in range(3): c()
+    assert c.peek() == 3
+    c.validate()
+    c.threshold = 1
+    assert_raises(ValueError, c.validate)
+
+    c2 = Counter(threshold = 2)
+    c2(); c2()
+    assert c2.peek() == 1
+    assert c2() == 2
+    c2.validate()
+    assert c2() == 0
+
+    c3 = Counter(resets = [c])
+    assert c() == 4
+    assert c3() == 0
+    assert c() == 0
+    assert c() == 1
+    assert c3() == 1
+    assert c() == 0
 
 #-------------------------------------------------------------------------------
 
--- syn/base/b/utils.py
+++ syn/base/b/utils.py
@@ -1,13 +1,60 @@
 from .base import Base
 from .meta import Attr
-from syn.base_utils import getitem
+
+#-------------------------------------------------------------------------------
+# Utilities
+
+Num = (int, float)
 
 #-------------------------------------------------------------------------------
 # Counter
 
 
 class Counter(Base):
-    pass
+    _attrs = dict(value = Attr(Num, default = -1, doc="The current count"),
+                  # TODO: replace with List(This) when that is available
+                  resets = Attr(list, init=lambda s: list(),
+                                doc="A list of counters to reset when this "\
+                                "counter is reset"),
+                  initial_value = Attr(Num, init=lambda self: self.value, 
+                                       doc="The initial "\
+                                       "value to which the counter is reset"),
+                  threshold = Attr(Num, optional=True, doc="Threshold at which"\
+                                   " to reset the counter"),
+                  step = Attr(Num, default = 1, 
+                              doc="Amount by which to increment the counter"),
+                )
+    _opts = dict(optional_none = True,
+                 init_validate = True)
+
+    def __call__(self, step=None):
+        if step is None:
+            step = self.step
+
+        self.value += step
+        ret = self.value
+
+        for counter in self.resets:
+            counter.reset()
+
+        if self.threshold is not None:
+            if self.value >= self.threshold:
+                self.reset()
+        
+        return ret
+
+    def peek(self):
+        return self.value
+
+    def reset(self):
+        self.value = self.initial_value
+
+    def validate(self):
+        super(Counter, self).validate()
+        
+        if self.threshold is not None:
+            if self.value >= self.threshold:
+                raise ValueError("value cannot be greater than threshold")
 
 
 #-------------------------------------------------------------------------------
