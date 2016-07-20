8853b47b3f4448d4171b27e6cc13085f9cf2ef2b
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-07-20 01:09:33 -0500

    Adding unit tests for syn.tree.b.Tree

--- syn/tree/b/tests/test_tree.py
+++ syn/tree/b/tests/test_tree.py
@@ -1,10 +1,204 @@
-from syn.tree.b import Tree
+from nose.tools import assert_raises
+from syn.tree.b import Tree, Node, TreeError, do_nothing
+from syn.base.b import check_idempotence
+from syn.base_utils import get_typename
+from syn.tree.b.tests.test_node import Tst2
 
 #-------------------------------------------------------------------------------
 # Tree
 
+#-----------------------------------------------------------
+# Tree Test 1
+
+def tree_tst_1(treecls):
+    cls = treecls._attrs.types['root'].type
+    clsname = get_typename(cls)
+    n1 = cls(_name='n1', _id=0)
+    n2 = cls(_name='n2', _id=1)
+    n3 = cls(_name='n3', _id=2)
+    n4 = cls(_name='n4', _id=3)
+    n5 = cls(_name='n5', _id=4)
+    n6 = cls(_name='n6', _id=5)
+
+    n1.add_child(n2)
+    n1.add_child(n3)
+
+    assert n1.siblings() == []
+    assert n2.siblings() == [n3]
+    assert n3.siblings() == [n2]
+
+    obj = treecls(n1)
+    check_idempotence(obj)
+
+    assert obj.nodes == [n1, n2, n3]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {0: n1, 1: n2, 2: n3}
+    assert obj.type_dict == {clsname: [n1, n2, n3]}
+
+    assert_raises(TreeError, obj.add_node, n3)
+    assert_raises(TreeError, obj.add_node, n4, parent=n5)
+    obj.add_node(n4, parent=n3)
+    
+    assert n4 in obj.nodes
+    assert n4 in n3._children
+    assert n4._parent is n3
+
+    assert_raises(TreeError, obj.add_node, n5, parent_id=100)
+    obj.add_node(n5, parent_id=1)
+
+    assert n5 in obj.nodes
+    assert n5 in n2._children
+    assert n5._parent is n2
+
+    obj.add_node(n6)
+    assert n6 in obj.nodes
+    assert n6 in n5._children
+    assert n6._parent is n5
+
+    assert obj.nodes == [n1, n2, n3, n4, n5, n6]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {0: n1, 1: n2, 2: n3, 3:n4, 4:n5, 5:n6}
+    assert obj.type_dict == {clsname: [n1, n2, n3, n4, n5, n6]}
+    
+    
+    for _id,node in enumerate([n1, n2, n3, n4, n5, n6]):
+        assert obj.get_node_by_id(_id) == obj._get_node_by_id(_id)
+        assert obj.get_node_by_id(_id) == node
+    assert obj.get_node_by_id(100) is None    
+    assert obj.get_node_by_id(-1) is None    
+    
+
+    assert_raises(TypeError, obj.depth_first, FooType = do_nothing)
+    assert_raises(TypeError, obj._check_search_kwarg_types, 
+                  {Tst2: do_nothing})
+    assert_raises(TypeError, obj._check_search_kwarg_types, {0: do_nothing})
+
+    assert n1.descendants() == [n2, n5, n6, n3, n4]
+
+    accum = []
+    def collect(node):
+        accum.append(node._id)
+    
+    obj.depth_first(collect)
+    assert accum == [0, 1, 4, 5, 2, 3]
+
+    accum = []
+    obj.depth_first(**{clsname: collect})
+    assert accum == [0, 1, 4, 5, 2, 3]
+
+    accum = []
+    obj.search_rootward(collect)
+    assert accum == [0]
+
+    accum = []
+    obj.search_rootward(**{'current_node': n5, 
+                           clsname: collect})
+    assert accum == [4, 1, 0]
+
+    def stop(node):
+        return node._id == 3
+    def get(node):
+        return node._name
+
+    assert obj.depth_first(stop_test = stop, _return = get) == 'n4'
+    assert obj.search_rootward(stop_test = stop, _return = get) is None
+    assert obj.search_rootward(current_node = n4, stop_test = stop, 
+                               _return = get) == 'n4'
+    assert obj.search_rootward(current_node = n6, stop_test = stop,
+                               _return = get) is None
+
+
+    n7 = cls(_name='n7', _id=6)
+    n8 = cls(_name='n8', _id=7)
+
+    n7.add_child(n8)
+    obj.replace_node(n5, n7)
+
+    assert n2._children == [n7]
+    assert n7._parent is n2
+    assert n7._children == [n8]
+    assert n8._parent is n7
+
+    assert n5._children == [n6]
+    assert n6._parent is n5
+    assert n5._parent is None
+
+    assert obj.nodes == [n1, n2, n3, n4, n7, n8]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {0: n1, 1: n2, 2: n3, 3:n4, 6:n7, 7:n8}
+    assert obj.type_dict == {clsname: [n1, n2, n3, n4, n7, n8]}
+
+
+    assert_raises(TreeError, obj.remove_node, n5)
+    assert_raises(TreeError, obj.replace_node, n5, n7)
+
+    obj.remove_node(n2)
+    assert n1._children == [n3]
+    assert n2._parent is None
+    assert n2._children == [n7]
+    assert n7._parent is n2
+
+    assert obj.nodes == [n1, n3, n4]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {0: n1, 2: n3, 3:n4}
+    assert obj.type_dict == {clsname: [n1, n3, n4]}
+
+
+    assert_raises(TreeError, obj.replace_node, n1, n7)
+    assert_raises(TreeError, obj.replace_node, n3, n7)
+
+    obj.replace_node(n1, n2)
+    assert n1._children == [n3]
+    assert n3._parent is n1
+
+    assert obj.root is n2
+    assert obj.nodes == [n2, n7, n8]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {1: n2, 6: n7, 7:n8}
+    assert obj.type_dict == {clsname: [n2, n7, n8]}
+    
+
+    obj.rebuild()
+    assert obj.root is n2
+    assert obj.nodes == [n2, n7, n8]
+    assert obj.node_types == [clsname]
+    assert obj.id_dict == {1: n2, 6: n7, 7:n8}
+    assert obj.type_dict == {clsname: [n2, n7, n8]}
+
+
+    obj.remove_node(n2)
+    assert obj.root is None
+    assert obj.nodes == []
+    assert obj.node_types == []
+    assert obj.id_dict == {}
+    assert obj.type_dict == {}
+
+#-----------------------------------------------------------
+# Tree Test 2
+
+def tree_tst_2(treecls):
+    pass
+
+#-----------------------------------------------------------
+# Tree
+
 def test_tree():
-    Tree
+    # Test that inequal roots mean inequal Trees
+    n1 = Node()
+    n2 = Node(_id=5)
+
+    t1 = Tree(n1)
+    t2 = Tree(n2)
+
+    assert n1 != n2
+    assert t1 != t2
+
+    t3 = Tree()
+    t3.validate()
+    assert t3 == t1
+
+    tree_tst_1(Tree)
+    tree_tst_2(Tree)
 
 #-------------------------------------------------------------------------------
 
--- syn/tree/b/tree.py
+++ syn/tree/b/tree.py
@@ -1,13 +1,190 @@
-from syn.base.b import Base, Attr
+from syn.five import STR
+from syn.type.a import List, Dict
+from syn.base.b import Base, Attr, Counter
+from .node import Node, TreeError
+from syn.base_utils import get_typename, getitem
 
+#-------------------------------------------------------------------------------
+# Default search functions
+
+def do_nothing(*args, **kwargs):
+    pass
+
+def identity(x):
+    return x
 
 #-------------------------------------------------------------------------------
 # Tree
 
 
 class Tree(Base):
-    _attrs = dict()
-    _opts = dict(init_validate = True)
+    _attrs = dict(root = Attr(Node, init=lambda self: Node(),
+                              doc="The root node of the tree"),
+                  nodes = Attr(List(Node), init=lambda self: list(),
+                               doc="List of all tree nodes"),
+                  node_types = Attr(List(STR), init=lambda self: list(),
+                                    doc="List of all tree node types"),
+                  id_dict = Attr(Dict(Node), init=lambda self: dict(),
+                                 doc="Mapping of ids to nodes"),
+                  type_dict = Attr(Dict(List(Node)), init=lambda self: dict(),
+                                   doc="Mapping of type names to node lists"),
+                  node_counter = Attr(Counter, init=lambda self: Counter(),
+                                      groups = ('eq_exclude',),
+                                      doc='Node id counter'),
+                 )
+    _opts = dict(init_validate = True,
+                 args = ('root',))
+
+    def __init__(self, *args, **kwargs):
+        super(Tree, self).__init__(*args, **kwargs)
+        self.add_node(self.root)
+
+    def add_node(self, node, **kwargs):
+        if node in self.nodes:
+            raise TreeError("Node '{}' already in tree; cannot add"
+                            .format(repr(node)))
+
+        def add_node(_node):
+            if _node._id is None:
+                _node._id = self.node_counter()
+
+            self.id_dict[_node._id] = _node
+            self.nodes.append(_node)
+            if get_typename(_node) not in self.node_types:
+                self.node_types.append(get_typename(_node))
+            if get_typename(_node) not in self.type_dict:
+                self.type_dict[get_typename(_node)] = []
+            self.type_dict[get_typename(_node)].append(_node)
+            
+        if node != self.root:
+            parent = getitem(kwargs, 'parent', None, True)
+            if not parent:
+                parent = self.get_node_by_id(getitem(kwargs, 'parent_id',
+                                                     max(self.id_dict)))
+
+            if parent not in self.nodes:
+                raise TreeError("Parent node '{}' not in tree" 
+                                .format(repr(parent)))
+            parent.add_child(node)
+
+        self.depth_first(node=add_node, current_node=node)
+
+    def remove_node(self, node, **kwargs):
+        if node not in self.nodes:
+            raise TreeError("Node '{}' not in tree; cannot remove" 
+                            .format(repr(node)))
+
+        def remove_node(_node):
+            del self.id_dict[_node._id]
+            self.nodes.remove(_node)
+            self.type_dict[get_typename(_node)].remove(_node)
+            if self.type_dict[get_typename(_node)] == []:
+                self.node_types.remove(get_typename(_node))
+                del self.type_dict[get_typename(_node)]
+
+        if node != self.root:
+            parent = node.get_parent()
+            if parent is None:
+                raise TreeError("Non-root node '{}' has no parent" 
+                                .format(repr(node)))
+            parent.remove_child(node)
+        else:
+            self.root = None
+
+        self.depth_first(node=remove_node, current_node=node)
+
+    def replace_node(self, source, dest, **kwargs):
+        if dest.get_parent() is not None:
+            raise TreeError("Node dest must have no parent")
+
+        parent = source.get_parent()
+        if parent is None:
+            if source == self.root:
+                self.remove_node(source, **kwargs)
+                self.root = dest
+                self.add_node(dest, **kwargs)
+            else:
+                raise TreeError("Non-root node '{}' has no parent"
+                                .format(repr(source)))
+        else:
+            self.remove_node(source, **kwargs)
+            self.add_node(dest, parent=parent, **kwargs)
+
+    def rebuild(self, **kwargs):
+        '''Repopulate the node-tracking data structures.  Shouldn't
+        really ever be needed.
+        '''
+        self.nodes = []
+        self.node_types = []
+        self.id_dict = {}
+        self.type_dict = {}
+
+        self.add_node(self.root)
+
+    def get_node_by_id(self, node_id):
+        ret = getitem(self.id_dict, node_id,
+                      self._get_node_by_id(node_id), True)
+        return ret
+
+    def _get_node_by_id(self, node_id):
+        for n in self.nodes:
+            if n._id == node_id:
+                return n
+        return None
+
+    def _check_search_kwarg_types(self, kwargs):
+        '''Checks that every element of kwargs is a valid type in this tree.'''
+        for key in kwargs:
+            if key not in self.node_types:
+                raise TypeError("Invalid search type: {}".format(key))
+
+    def depth_first(self, node = do_nothing,
+                    stop_test = do_nothing,
+                    _return = identity,
+                    current_node = 'root',
+                    **kwargs):
+
+        self._check_search_kwarg_types(kwargs)
+        if current_node == 'root':
+            current_node = self.root
+
+        node(current_node)
+        if stop_test(current_node):
+            return _return(current_node)
+
+        for nodetype, function in kwargs.items():
+            if get_typename(current_node) == nodetype:
+                function(current_node)
+                
+        for child in current_node.get_children():
+            ret = self.depth_first(node, stop_test, _return, child, **kwargs)
+            if ret:
+                return ret
+
+    def search_rootward(self, node = do_nothing,
+                        stop_test = do_nothing,
+                        _return = identity,
+                        current_node = 'root',
+                        **kwargs):
+        
+        self._check_search_kwarg_types(kwargs)
+        
+        if current_node == 'root':
+            current_node = self.root
+
+        node(current_node)
+        if stop_test(current_node):
+            return _return(current_node)
+
+        for nodetype, function in kwargs.items():
+            if get_typename(current_node) == nodetype:
+                function(current_node)
+
+        if current_node is self.root:
+            return
+
+        parent = current_node.get_parent()
+        return self.search_rootward(node, stop_test, _return, parent, **kwargs)
 
     def validate(self):
         super(Tree, self).validate()
@@ -17,6 +194,6 @@ class Tree(Base):
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('Tree',)
+__all__ = ('Tree', 'do_nothing', 'identity')
 
 #-------------------------------------------------------------------------------
