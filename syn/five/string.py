c06e267be1cf27cb2c34c58f47a0e4eff4aece12
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-09 00:18:40 -0500

    Adding unicode and unichr to syn.five exports

--- syn/five/string.py
+++ syn/five/string.py
@@ -4,12 +4,16 @@ STR = six.string_types
 
 if six.PY2:
     strf = unicode
+    unicode = unicode
+    unichr = unichr
 else:
     strf = str
+    unicode = str
+    unichr = chr
 
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('STR', 'strf')
+__all__ = ('STR', 'strf', 'unicode', 'unichr')
 
 #-------------------------------------------------------------------------------
