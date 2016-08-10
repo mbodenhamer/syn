16b1292f7dae54b635e16b7806ed1d2261aa5bad
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-10 16:38:44 -0500

    Attempting to make feq() default tolerance machine-independent

--- syn/base_utils/float.py
+++ syn/base_utils/float.py
@@ -1,9 +1,15 @@
+import sys
+import math
 from syn.five import NUM
+
+#-------------------------------------------------------------------------------
+
+DEFAULT_TOLERANCE = math.pow(10, math.log(sys.float_info.epsilon, 10) / 2.0)
 
 #-------------------------------------------------------------------------------
 # Comparison
 
-def feq(a, b, tol=1e-8):
+def feq(a, b, tol=DEFAULT_TOLERANCE):
     if isinstance(a, NUM) and isinstance(b, NUM):
         ret = abs(abs(a) - abs(b)) < tol
         return ret
