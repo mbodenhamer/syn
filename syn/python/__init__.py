7fc7f9181894016680609ca548c045605f7d71f7
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-16 18:45:06 -0500

    Adding more metadata harvesting

---
+++ syn/python/__init__.py
@@ -0,0 +1,5 @@
+from .b import *
+
+from syn.base_utils import harvest_metadata, delete
+with delete(harvest_metadata, delete):
+    harvest_metadata('../metadata.yml')
---
+++ syn/serialize/__init__.py
@@ -0,0 +1,3 @@
+from syn.base_utils import harvest_metadata, delete
+with delete(harvest_metadata, delete):
+    harvest_metadata('../metadata.yml')
