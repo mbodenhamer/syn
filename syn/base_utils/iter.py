35fa0300a720cf19e262b42e19e3a9f73026d17a
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-25 17:59:58 -0500

    Adding iterlen and is_empty

--- syn/base_utils/iter.py
+++ syn/base_utils/iter.py
@@ -1,5 +1,23 @@
+from itertools import islice
+from collections import Iterator
+
+#-------------------------------------------------------------------------------
+
+def iterlen(iter):
+    '''Returns the number of iterations remaining over iter.
+    '''
+    if not isinstance(iter, Iterator):
+        return len(iter)
+    return iter.__length_hint__()
+
+def is_empty(iter):
+    '''Returns True if iter is empty, otherwise False.
+    '''
+    return iterlen(iter) == 0
+
 def consume(iter, N=None):
-    from itertools import islice
+    '''Consumes N items from iter.  If N is None (or not given), consumes all.
+    '''
     if N is None:
         list(iter)
     else:
@@ -8,6 +26,6 @@ def consume(iter, N=None):
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('consume',)
+__all__ = ('consume', 'iterlen', 'is_empty')
 
 #-------------------------------------------------------------------------------
--- syn/base_utils/tests/test_iter.py
+++ syn/base_utils/tests/test_iter.py
@@ -1,9 +1,20 @@
+from nose.tools import assert_raises
 from syn.five import range
 
 #-------------------------------------------------------------------------------
 
+def test_iterlen():
+    from syn.base_utils import iterlen, is_empty
+    l = range(10)
+    assert iterlen(l) == 10
+    i = iter(l)
+    assert_raises(TypeError, len, i)
+    assert iterlen(i) == 10
+    assert not is_empty(i)
+    assert iterlen(i) == 10
+
 def test_consume():
-    from syn.base_utils import consume
+    from syn.base_utils import consume, is_empty
 
     lst = range(10)
     i1 = iter(lst)
@@ -11,7 +22,7 @@ def test_consume():
     lst2 = list(i1)
     assert lst2 == range(4, 10)
     consume(i1)
-    # assert is_empty(i1)
+    assert is_empty(i1)
 
 #-------------------------------------------------------------------------------
 
