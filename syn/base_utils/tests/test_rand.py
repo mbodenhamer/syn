575c5b3572a0a746d471d7948d534fe3e3727e4f
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-10-24 21:37:40 -0500

    Improving test_rand.py

--- syn/base_utils/tests/test_rand.py
+++ syn/base_utils/tests/test_rand.py
@@ -2,12 +2,18 @@ from nose.tools import assert_raises
 from syn.five import xrange, PY2, PY3
 
 from syn.base_utils.rand import MIN_FLOAT, PRIMITIVE_TYPES
-from syn.base_utils import is_hashable, ngzwarn
+from syn.base_utils import is_hashable, ngzwarn, on_error, elog
 
 from syn.globals import TEST_SAMPLES as SAMPLES
-SAMPLES //= 2
+SAMPLESII = SAMPLES // 2
+SAMPLESIII = SAMPLES // 5
+
 SAMPLES = max(SAMPLES, 1)
+SAMPLESII = max(SAMPLESII, 1)
+SAMPLESIII = max(SAMPLESIII, 1)
 ngzwarn(SAMPLES, 'SAMPLES')
+ngzwarn(SAMPLES, 'SAMPLESII')
+ngzwarn(SAMPLES, 'SAMPLESIII')
 
 #-------------------------------------------------------------------------------
 # Numeric
@@ -15,44 +21,47 @@ ngzwarn(SAMPLES, 'SAMPLES')
 def test_rand_bool():
     from syn.base_utils import rand_bool
 
-    bools = [rand_bool() for k in xrange(SAMPLES)]
+    bools = [rand_bool() for k in xrange(SAMPLESIII)]
     assert all(isinstance(s, bool) for s in bools)
 
 def test_randint():
     from syn.base_utils import rand_int
 
-    ints = [rand_int() for k in xrange(SAMPLES)]
+    ints = [rand_int() for k in xrange(SAMPLESII)]
     assert all(isinstance(i, int) for i in ints)
-    assert any(i != 0 for i in ints)
+    if len(ints) > 2:
+        assert any(i != 0 for i in ints)
 
 def test_randlong():
     from syn.base_utils import rand_long
 
     if PY2:
-        longs = [rand_long() for k in xrange(SAMPLES)]
+        longs = [rand_long() for k in xrange(SAMPLESII)]
         assert all(isinstance(l, long) for l in longs)
-        assert any(l != 0 for l in longs)
+        if len(longs) > 2:
+            assert any(l != 0 for l in longs)
 
 def test_randfloat():
     from syn.base_utils import rand_float
     
-    floats = [rand_float() for k in xrange(SAMPLES)]
+    floats = [rand_float() for k in xrange(SAMPLESII)]
     assert all(isinstance(f, float) for f in floats)
-    assert any(f != 0 for f in floats)
+    if len(floats) > 2:
+        assert any(f != 0 for f in floats)
 
-    floats = [rand_float(ub = 3) for k in xrange(SAMPLES)]
+    floats = [rand_float(ub = 3) for k in xrange(SAMPLESII)]
     assert all(MIN_FLOAT <= f <= 3 for f in floats)
 
-    floats = [rand_float(2, 3) for k in xrange(SAMPLES)]
+    floats = [rand_float(2, 3) for k in xrange(SAMPLESII)]
     assert all(2 <= f <= 3 for f in floats)
 
 def test_rand_complex():
     from syn.base_utils import rand_complex
 
-    comps = [rand_complex() for k in xrange(SAMPLES)]
+    comps = [rand_complex() for k in xrange(SAMPLESIII)]
     assert all(isinstance(s, complex) for s in comps)
 
-    comps = [rand_complex(imag_only=True) for k in xrange(SAMPLES)]
+    comps = [rand_complex(imag_only=True) for k in xrange(SAMPLESII)]
     assert all(isinstance(s, complex) for s in comps)
     assert all(s.real == 0 for s in comps)
 
@@ -62,24 +71,26 @@ def test_rand_complex():
 def test_randstr():
     from syn.base_utils import rand_str
 
-    strs = [rand_str() for k in xrange(SAMPLES)]
+    strs = [rand_str() for k in xrange(SAMPLESII)]
     assert all(isinstance(s, str) for s in strs)
-    assert any(len(s) > 0 for s in strs)
+    if len(strs) > 2:
+        assert any(len(s) > 0 for s in strs)
 
 
 def test_rand_unicode():
     from syn.base_utils import rand_unicode
 
     if PY2:
-        strs = [rand_unicode(min_len=1) for k in xrange(SAMPLES)]
+        strs = [rand_unicode(min_len=1) for k in xrange(SAMPLESII)]
         assert all(isinstance(s, unicode) for s in strs)
-        assert any(len(s) > 0 for s in strs)
+        if len(strs) > 2:
+            assert any(len(s) > 0 for s in strs)
 
 def test_rand_bytes():
     from syn.base_utils import rand_bytes
     
     if PY3:
-        bytess = [rand_bytes() for k in xrange(SAMPLES)]
+        bytess = [rand_bytes() for k in xrange(SAMPLESII)]
         assert all(isinstance(b, bytes) for b in bytess)
 
 #-------------------------------------------------------------------------------
@@ -88,17 +99,19 @@ def test_rand_bytes():
 def test_rand_list():
     from syn.base_utils import rand_list, is_flat
 
-    lists = [rand_list() for k in xrange(SAMPLES)]
+    lists = [rand_list() for k in xrange(SAMPLESIII)]
     assert all(isinstance(l, list) for l in lists)
 
-    for k in xrange(SAMPLES):
+    for k in xrange(SAMPLESII):
         l = rand_list(max_depth=0)
-        assert is_flat(l)
+
+        with on_error(elog, is_flat, (l,)):
+            assert is_flat(l)
 
 def test_rand_tuple():
     from syn.base_utils import rand_tuple
 
-    tuples = [rand_tuple() for k in xrange(SAMPLES)]
+    tuples = [rand_tuple() for k in xrange(SAMPLESIII)]
     assert all(isinstance(t, tuple) for t in tuples)
 
 #-------------------------------------------------------------------------------
@@ -107,7 +120,7 @@ def test_rand_tuple():
 def test_rand_dict():
     from syn.base_utils import rand_dict
 
-    dicts = [rand_dict() for k in xrange(SAMPLES)]
+    dicts = [rand_dict() for k in xrange(SAMPLESIII)]
     assert all(isinstance(d, dict) for d in dicts)
 
 #-------------------------------------------------------------------------------
@@ -116,13 +129,13 @@ def test_rand_dict():
 def test_rand_set():
     from syn.base_utils import rand_set
 
-    sets = [rand_set() for k in xrange(SAMPLES)]
+    sets = [rand_set() for k in xrange(SAMPLESIII)]
     assert all(isinstance(s, set) for s in sets)
     
 def test_rand_frozenset():
     from syn.base_utils import rand_frozenset
 
-    sets = [rand_frozenset() for k in xrange(SAMPLES)]
+    sets = [rand_frozenset() for k in xrange(SAMPLESIII)]
     assert all(isinstance(s, frozenset) for s in sets)
 
 #-------------------------------------------------------------------------------
@@ -131,7 +144,7 @@ def test_rand_frozenset():
 def test_rand_none():
     from syn.base_utils import rand_none
 
-    nones = [rand_none() for k in range(SAMPLES)]
+    nones = [rand_none() for k in range(SAMPLESIII)]
     assert all(s is None for s in nones)
 
 #-------------------------------------------------------------------------------
@@ -172,7 +185,7 @@ def test_rand_dispatch():
 def test_rand_primitive():
     from syn.base_utils import rand_primitive
 
-    for k in xrange(SAMPLES):
+    for k in xrange(SAMPLESII):
         x = rand_primitive()
         assert isinstance(x, tuple(PRIMITIVE_TYPES))
 
@@ -181,7 +194,9 @@ def test_rand_hashable():
 
     for k in xrange(SAMPLES):
         x = rand_hashable()
-        assert is_hashable(x)
+
+        with on_error(elog, is_hashable, (x,)):
+            assert is_hashable(x)
 
 #-------------------------------------------------------------------------------
 
