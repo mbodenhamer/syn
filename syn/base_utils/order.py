4d7189870436682c75c7d627d0ba6e0e6953326a
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-02-21 03:17:01 -0600

    Adding topological_sorting()

--- syn/base_utils/order.py
+++ syn/base_utils/order.py
@@ -1,12 +1,96 @@
+from collections import defaultdict
+
+#-------------------------------------------------------------------------------
+# Special Orders
+
+class First(object):
+    pass
+
+class Last(object):
+    pass
+
+#-------------------------------------------------------------------------------
+# Order Relations
+
+
+class LE(object):
+    def __init__(self, A, B):
+        self.A = A
+        self.B = B
+
+
+#-------------------------------------------------------------------------------
+# Utilities
+
+def _nodes(relations):
+    ret = set()
+    for rel in relations:
+        ret.add(rel.A)
+        ret.add(rel.B)
+    return ret
+
+def _incoming(relations):
+    ret = defaultdict(set)
+    for rel in relations:
+        ret[rel.B].add(rel.A)
+    return ret
+
+def _outgoing(relations):
+    ret = defaultdict(set)
+    for rel in relations:
+        ret[rel.A].add(rel.B)
+    return ret
+
+def _is_free(node, edges):
+    return not edges[node]
+
+def _free_nodes(nodes, edges):
+    return {node for node in nodes if _is_free(node, edges)}
+
 #-------------------------------------------------------------------------------
 # Topological Sort
 
-def topological_sort():
-    pass
+def topological_sorting(relations):
+    '''An implementation of Kahn's algorithm.
+    '''
+    ret = []
+    nodes = _nodes(relations)
+    inc = _incoming(relations)
+    out = _outgoing(relations)
+    free = _free_nodes(nodes, inc)
+
+    if First in nodes:
+        if not _is_free(First, inc):
+            raise ValueError("Node First has incoming edge")
+    
+    if Last in nodes:
+        if not _is_free(Last, out):
+            raise ValueError("Node Last has outgoing edge")
+
+    while free:
+        n = free.pop()
+        ret.append(n)
+        
+        out_n = list(out[n])
+        for m in out_n:
+            out[n].remove(m)
+            inc[m].remove(n)
+            if _is_free(m, inc):
+                free.add(m)
+
+    if not all(_is_free(node, inc) and _is_free(node, out) for node in nodes):
+        raise ValueError("Cycle detected")
+
+    if First in ret:
+        ret.remove(First)
+    if Last in ret:
+        ret.remove(Last)
+    return ret
 
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('topological_sort',)
+__all__ = ('First', 'Last', 'LE',
+           'topological_sorting',)
 
 #-------------------------------------------------------------------------------
--- syn/base_utils/tests/test_order.py
+++ syn/base_utils/tests/test_order.py
@@ -1,8 +1,28 @@
+from nose.tools import assert_raises
+
 #-------------------------------------------------------------------------------
 # Topological Sort
 
-def test_topological_sort():
-    from syn.base_utils import topological_sort
+def test_topological_sorting():
+    from syn.base_utils import topological_sorting, First, Last, LE
+
+    assert topological_sorting([LE(1, 2)]) == [1, 2]
+    assert topological_sorting([LE(First, 1)]) == [1]
+    assert topological_sorting([LE(1, Last)]) == [1]
+
+    rels = [LE(First, 1),
+            LE(1, 2),
+            LE(2, 3),
+            LE(3, Last)]
+    assert topological_sorting(rels) == [1, 2, 3]
+
+    cycle = [LE(1, 2),
+             LE(2, 3),
+             LE(3, 2)]
+    assert_raises(ValueError, topological_sorting, cycle)
+
+    assert_raises(ValueError, topological_sorting, [LE(1, First)])
+    assert_raises(ValueError, topological_sorting, [LE(Last, 1)])
 
 #-------------------------------------------------------------------------------
 
