dfa30c0e6c1b9d1b413e596ffa02429ed82616e0
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-03-20 01:27:15 -0500

    Adding Assign

--- syn/python/b/statements.py
+++ syn/python/b/statements.py
@@ -1,16 +1,50 @@
-from .base import PythonNode
+from .base import PythonNode, Attr, AST, ACO, col_offset, from_ast
+from syn.base_utils import setitem
+from syn.type.a import List
 
 #-------------------------------------------------------------------------------
 # Statement
 
 
 class Statement(PythonNode):
-    pass
+    _opts = dict(max_len = 0)
+
+
+#-------------------------------------------------------------------------------
+# Assign
+
+
+class Assign(Statement):
+    _attrs = dict(targets = Attr(List(PythonNode)),
+                  value = Attr(PythonNode))
+    _opts = dict(args = ('targets', 'value'))
+
+    def emit(self, **kwargs):
+        with setitem(kwargs, 'col_offset', 0):
+            targs = [targ.emit(**kwargs) for targ in self.targets]
+            val = self.value.emit(**kwargs)
+
+        ret = ' ' * col_offset(self, kwargs)
+        ret += ' = '.join(targs)
+        ret += ' = ' + val
+        return ret
+
+    @classmethod
+    def from_ast(cls, ast, **kwargs):
+        targs = [from_ast(targ, **kwargs) for targ in ast.targets]
+        val = from_ast(ast.value, **kwargs)
+        return cls(targs, val)
+
+    def to_ast(self, **kwargs):
+        targs = [targ.to_ast(**kwargs) for targ in self.targets]
+        val = self.value.to_ast(**kwargs)
+        return self.ast(targs, val)
 
 
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('Statement',)
+__all__ = ('Statement',
+           'Assign')
 
 #-------------------------------------------------------------------------------
--- syn/python/b/tests/test_statements.py
+++ syn/python/b/tests/test_statements.py
@@ -1,10 +1,24 @@
-from syn.python.b import Statement
+from functools import partial
+from syn.base_utils import compose
+from syn.python.b import Statement, from_source, from_ast
+
+mparse = compose(partial(from_source, mode='exec'), str)
 
 #-------------------------------------------------------------------------------
 # Statement
 
 def test_statement():
     Statement
+
+#-------------------------------------------------------------------------------
+# Assign
+
+def test_assign():
+    tree = mparse('a = 1')
+    assert tree.emit() == 'a = 1'
+    tree2 = from_ast(tree.to_ast(), mode='exec')
+    assert tree2.emit() == 'a = 1'
+
 
 #-------------------------------------------------------------------------------
 
