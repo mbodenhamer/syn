8c6d72de128b05be0eb8f78e54b3eb9d0a75c761
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-03-22 16:35:45 -0500

    Adding If

--- syn/python/b/blocks.py
+++ syn/python/b/blocks.py
@@ -1,4 +1,6 @@
-from .base import PythonNode
+from syn.base_utils import setitem
+from syn.type.a import List
+from .base import PythonNode, Attr, from_ast, col_offset
 
 
 #-------------------------------------------------------------------------------
@@ -6,12 +8,66 @@ from .base import PythonNode
 
 
 class Block(PythonNode):
-    pass
+    _attrs = dict(body = Attr(List(PythonNode)),
+                  indent = Attr(int, 4))
+    _opts = dict(max_len = 0)
+
+    def emit_block(self, head, body, **kwargs):
+        block_offset = col_offset(self, kwargs)
+        ret = ' ' * block_offset
+        ret += head + ':\n'
+
+        body_offset = body[0].col_offset if body[0].col_offset is not None else 0
+        body_offset = (body_offset if body_offset > block_offset else 
+                       block_offset + self.indent)
+
+        with setitem(kwargs, 'col_offset', body_offset):
+            strs = [elem.emit(**kwargs) for elem in body]
+
+        ret += '\n'.join(strs)
+        return ret
+
+
+#-------------------------------------------------------------------------------
+# If
+
+
+class If(Block):
+    _attrs = dict(test = Attr(PythonNode),
+                  orelse = Attr(List(PythonNode)))
+    _opts = dict(args = ('test', 'body', 'orelse'))
+
+    def emit(self, **kwargs):
+        with setitem(kwargs, 'col_offset', 0):
+            head = 'if ' + self.test.emit(**kwargs)
+
+        ret = self.emit_block(head, self.body, **kwargs)
+        
+        if self.orelse:
+            head = 'else:'
+            block = self.emit_block(head, self.orelse, **kwargs)
+            ret += '\n' + block
+
+        return ret
+
+    @classmethod
+    def from_ast(cls, ast, **kwargs):
+        test = from_ast(ast.test, **kwargs)
+        body = [from_ast(elem, **kwargs) for elem in ast.body]
+        orelse = [from_ast(elem, **kwargs) for elem in ast.orelse]
+        return cls(test, body, orelse)
+
+    def to_ast(self, **kwargs):
+        test = self.test.to_ast(**kwargs)
+        body = [elem.to_ast(**kwargs) for elem in self.body]
+        orelse = [elem.to_ast(**kwargs) for elem in self.orelse]
+        return self.ast(test, body, orelse)
 
 
 #-------------------------------------------------------------------------------
 # __all__
 
-__all__ = ('Block',)
+__all__ = ('Block',
+           'If')
 
 #-------------------------------------------------------------------------------
